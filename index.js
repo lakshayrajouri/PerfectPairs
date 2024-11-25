25d694cb0b5ea592e47991a2cc654cf7d0de80c0
lakshayrajouri <70949963+lakshayrajouri@users.noreply.github.com>
2024-11-25 23:55:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const pool = new Pool({
     user: 'postgres',    // Replace with your PostgreSQL username
     host: 'localhost',        // Replace with your database host
     database: 'PerfectPairs_db', // Replace with your database name
-    password: '12345',    // Replace with your PostgreSQL password
+    password: '****',    // Replace with your PostgreSQL password
     port: 5432,               // Default PostgreSQL port
 });
 
